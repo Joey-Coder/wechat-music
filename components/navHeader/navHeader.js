6e58454d373e8bb2e689399944889e8f45e21e7a
Joey-Coder <joey950326@gmail.com>
2021-02-28 14:00:00 +0800

    实现header组件分离，复用

---
+++ components/navHeader/navHeader.js
@@ -0,0 +1,30 @@
+// components/aaa/aaa.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    nav: {
+      type: String,
+      value: 'nav'
+    },
+    title: {
+      type: String,
+      value: 'title'
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
